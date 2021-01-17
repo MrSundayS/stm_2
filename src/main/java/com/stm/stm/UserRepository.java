c2279b0ddb37093b1a968008554146bf59e4cb61
Alvaro Domingo <seandeblin@hotmail.com>
2021-01-17 13:23:49 +0100

    method to create user implemented

---
+++ src/main/java/com/stm/stm/UserRepository.java
@@ -0,0 +1,9 @@
+package com.stm.stm;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findFirstByEmail(String email);    // -> SELECT * FROM user WHERE email = ?;
+}
